f6049de9ffa64be0d307c9547c38244c74f98714
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-11-18 11:45:37 +0100

    stock_quant_package_product_packaging: imp reset package type
    Only reset the package type of disposable package.
    In this way reusable package will keep their type.

--- stock_quant_package_product_packaging/models/stock_quant_package.py
+++ stock_quant_package_product_packaging/models/stock_quant_package.py
@@ -90,5 +90,7 @@ class StockQuantPackage(models.Model):
                 package.package_type_id = product.package_type_id
 
     def _reset_empty_package_package_type(self):
-        empty_packages = self.filtered(lambda rec: not rec.quant_ids)
+        empty_packages = self.filtered(
+            lambda rec: not rec.quant_ids and rec.package_use == "disposable"
+        )
         empty_packages.package_type_id = False
